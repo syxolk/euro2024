e7be717cb2f5fdbb032883e9070aea52eefb7ddc
Hans Kirchner <hans-kirchner@gmx.de>
2016-04-05 11:47:39 +0200

    Add simple URL fingerprinting

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const compression = require('compression');
 const moment = require('moment');
 const csrf = require('csurf');
 const fs = require('fs');
+const package = require('./package.json');
 const routes = require('./routes');
 const config = require('./config');
 const instance = require('./models').instance;
@@ -36,6 +37,16 @@ app.enable('case sensitive routing');
 
 app.locals.origin = config.origin;
 hbs.localsAsTemplateData(app);
+
+if(process.env.NODE_ENV === 'production') {
+    hbs.registerHelper('asset', function(path) {
+        return '/' + package.version + path;
+    });
+} else {
+    hbs.registerHelper('asset', function(path) {
+        return path;
+    });
+}
 
 hbs.registerHelper('calendar', function(when) {
     return moment(when).format('dddd, MMMM Do, H:mm');
@@ -69,8 +80,13 @@ hbs.registerHelper('scoreClass', function(match) {
 });
 
 app.use(compression());
-app.use(express.static(__dirname + '/bower_components', { maxAge: '1d' }));
-app.use(express.static(__dirname + '/public'));
+if(process.env.NODE_ENV === 'production') {
+    app.use('/' + package.version, express.static(__dirname + '/bower_components', {maxAge: '365d'}));
+    app.use('/' + package.version, express.static(__dirname + '/public', {maxAge: '365d'}));
+} else {
+    app.use(express.static(__dirname + '/bower_components'));
+    app.use(express.static(__dirname + '/public'));
+}
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
     name: 'sid',
