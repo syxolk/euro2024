cf86b6c1ebf0b0fb18af07c374d5a5dcb5f3c349
Hans Kirchner <hans-kirchner@gmx.de>
2016-05-26 02:15:26 +0200

    Add 200x200 favicon, swap / with /login

--- routes/login.js
+++ routes/login.js
@@ -31,11 +31,11 @@ passport.use(new LocalStrategy({
 ));
 
 module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.redirect('/login');
+    app.get('/login', function(req, res) {
+        res.redirect('/');
     });
 
-    app.get('/login', function(req, res) {
+    app.get('/', function(req, res) {
         res.render('login', {
             csrfToken: req.csrfToken(),
             loggedIn: !!req.user,
