90e02376be64646e252ba2b25fc3e3eade3e3756
AlexanderSachin <alexandros.sachin@gmail.com>
2016-06-13 15:52:27 +0200

    Highscore is now also ordered by number of stars in case of ties

--- routes/highscore.js
+++ routes/highscore.js
@@ -2,14 +2,14 @@ const instance = require('../models').instance;
 
 module.exports = function(app) {
     app.get('/highscore', function(req, res) {
-        var orderBy = 'score';
-        var orderDir = 'DESC';
+        var orderBy = 'score DESC, count3 DESC, count2 DESC, count1 DESC, count0 DESC, name ASC';
+	var orderField = req.query.order;
         switch(req.query.order) {
-            case 'name': orderBy = 'name'; orderDir = 'ASC'; break;
-            case '3': orderBy = 'count3'; break;
-            case '2': orderBy = 'count2'; break;
-            case '1': orderBy = 'count1'; break;
-            case '0': orderBy = 'count0'; break;
+            case 'name': orderBy = 'name ASC'; orderField = 'name'; break;
+            case '3': orderBy = 'count3 DESC, score DESC, count2 DESC, count1 DESC, count0 ASC, name ASC'; break;
+            case '2': orderBy = 'count2 DESC, score DESC, count3 DESC, count1 DESC, count0 ASC, name ASC'; break;
+            case '1': orderBy = 'count1 DESC, score DESC, count3 DESC, count2 DESC, count0 ASC, name ASC'; break;
+            case '0': orderBy = 'count0 DESC, score DESC, count3 DESC, count2 DESC, count1 DESC, name ASC'; break;
         }
         const onlyFriends = req.query.friends === '1';
 
@@ -17,7 +17,7 @@ module.exports = function(app) {
             id = $id as isme, id in (SELECT "ToUserId" FROM "Friend" WHERE "FromUserId" = $id) as isfriend,
             rank() over (order by score desc) as rank FROM score_table` +
             (onlyFriends ? ' WHERE id = $id OR id in (SELECT "ToUserId" FROM "Friend" WHERE "FromUserId" = $id)' : '') +
-            ' ORDER BY ' + orderBy + ' ' + orderDir + ', name ASC',
+            ' ORDER BY ' + orderBy,
             {
                 type: instance.QueryTypes.SELECT,
                 bind: {id: (req.user ? req.user.id : 0)}
@@ -28,12 +28,12 @@ module.exports = function(app) {
                 users: results,
                 loggedIn: !!req.user,
                 loggedUser: req.user,
-                orderScore: orderBy === 'score',
-                orderName: orderBy === 'name',
-                order3: orderBy === 'count3',
-                order2: orderBy === 'count2',
-                order1: orderBy === 'count1',
-                order0: orderBy === 'count0',
+                orderScore: orderField === '',
+                orderName: orderField === 'name',
+                order3: orderField === '3',
+                order2: orderField === '2',
+                order1: orderField === '1',
+                order0: orderField === '0',
                 friends: onlyFriends,
             });
         });
