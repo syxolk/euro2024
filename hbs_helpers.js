99b6487becfd53bbfca59c6e368b13b14ff93256
Hans Kirchner <hans-kirchner@gmx.de>
2018-05-22 13:34:31 +0200

    Add more general hsb helpers

--- hbs_helpers.js
+++ hbs_helpers.js
@@ -49,4 +49,31 @@ module.exports = () => {
     hbs.registerHelper('lt0', function(val) {
         return val < 0;
     });
+
+    hbs.registerHelper({
+        eq: function (v1, v2) {
+            return v1 === v2;
+        },
+        ne: function (v1, v2) {
+            return v1 !== v2;
+        },
+        lt: function (v1, v2) {
+            return v1 < v2;
+        },
+        gt: function (v1, v2) {
+            return v1 > v2;
+        },
+        lte: function (v1, v2) {
+            return v1 <= v2;
+        },
+        gte: function (v1, v2) {
+            return v1 >= v2;
+        },
+        and: function () {
+            return Array.prototype.slice.call(arguments, 0, -1).every(Boolean);
+        },
+        or: function () {
+            return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+        }
+    });
 };
