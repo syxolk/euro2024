d29001076f07452d0febed3c3a7130f9337c8bce
Hans Kirchner <hans-kirchner@gmx.de>
2021-06-20 21:38:09 +0200

    Fix backup script

--- backup-daemon/backup.sh
+++ backup-daemon/backup.sh
@@ -2,5 +2,5 @@
 set -e
 
 NOW=$(date +'%Y%m%d_%H%M%S')
-BACKUP_NAME="/backups/db_$NOW.sql.gz"
+BACKUP_FILE="/backups/db_$NOW.sql.gz"
 PGPASSWORD=123456 pg_dump -h db -U euro2020 euro2020 | gzip > "$BACKUP_FILE"
