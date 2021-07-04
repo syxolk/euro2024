d00a98187c30b0b1c22fba7a5fd1de94df2b766a
Hans Kirchner <hans-kirchner@gmx.de>
2021-07-05 01:09:01 +0200

    Improve past matches page
    - show own score
    - show average friend score
    - show gain
    - allow to order by gain

--- routes/past.js
+++ routes/past.js
@@ -3,37 +3,100 @@ const router = require("express-promise-router")();
 module.exports = router;
 
 router.get("/past", async (req, res) => {
+    const orderByGain = req.query.gain === "1";
+
+    const orderBy = orderByGain
+        ? "my_gain DESC, match.starts_at DESC"
+        : "match.starts_at DESC";
+
     const matches = await knex.raw(
         `
             SELECT match.id as id,
             match.starts_at as starts_at,
-            (SELECT name FROM match_type WHERE match_type.id = match.match_type_id) as matchtype,
-            (SELECT score_factor FROM match_type WHERE match_type.id = match.match_type_id) as score_factor,
-             match.goals_home as goalshome,
-             match.goals_away as goalsaway,
-            (SELECT name FROM team WHERE team.id = match.home_team_id) as hometeam,
-            (SELECT name FROM team WHERE team.id = match.away_team_id) as awayteam,
-            array_agg(bet.goals_home order by user_account.name asc) as listhome,
-            array_agg(bet.goals_away order by user_account.name asc) as listaway,
-            array_agg(user_account.name order by user_account.name asc) as listname,
-            array_agg(user_account.id order by user_account.name asc) as listid,
-            array_agg(calc_bet_result(match.goals_home, match.goals_away, bet.goals_home, bet.goals_away)::text order by user_account.name asc) as listresult,
-            array_agg(user_account.id in (SELECT to_user_id FROM friend WHERE from_user_id = :id) order by user_account.name asc) as listfriends,
-            array_agg(user_account.id = :id order by user_account.name asc) as listme,
-            count(bet.id) as countbets,
-            round(100.0 * count(CASE WHEN bet.goals_home > bet.goals_away THEN 1 END) / count(bet.id)) as winnerhome,
-            round(100.0 * count(CASE WHEN bet.goals_home < bet.goals_away THEN 1 END) / count(bet.id)) as winneraway,
-            avg(bet.goals_home) as avghome,
-            avg(bet.goals_away) as avgaway,
+            match_type.name as matchtype,
+            match_type.score_factor as score_factor,
+            match.goals_home as goalshome,
+            match.goals_away as goalsaway,
+            home_team.name as hometeam,
+            away_team.name as awayteam,
+            (
+                select array_agg(
+                    jsonb_build_object(
+                        'goals_home', bet.goals_home,
+                        'goals_away', bet.goals_away,
+                        'name', user_account.name,
+                        'id', user_account.id,
+                        'result', calc_bet_result(match.goals_home, match.goals_away, bet.goals_home, bet.goals_away)::text,
+                        'is_friend', user_account.id in (SELECT to_user_id FROM friend WHERE from_user_id = :id),
+                        'is_me', user_account.id = :id
+                    )
+                )
+                from bet
+                join user_account on (bet.user_id = user_account.id)
+                where bet.match_id = match.id
+            ) as all_bets,
+
+            (
+                select jsonb_build_object(
+                    'total', count(1),
+                    'home', count(1) filter (where bet.goals_home > bet.goals_away),
+                    'away', count(1) filter (where bet.goals_home < bet.goals_away),
+                    'draw', count(1) filter (where bet.goals_home = bet.goals_away)
+                )
+                from bet
+                where bet.match_id = match.id
+            ) as bet_counts,
+
             :logged_in and (goals_inserted_at > :last_visited or :last_visited::timestamptz is null) as unseen,
-            goals_inserted_at
+            goals_inserted_at,
+
+            (
+                select round(avg(calc_bet_score(
+                    calc_bet_result(
+                        match.goals_home, match.goals_away, friend_bet.goals_home, friend_bet.goals_away
+                    ),
+                    match_type.score_factor
+                    )), 1)
+                from friend
+                join bet as friend_bet on (friend_bet.user_id = friend.to_user_id)
+                where friend.from_user_id = :id
+            ) as avg_friend_score,
+
+            calc_bet_score(
+                calc_bet_result(
+                    match.goals_home, match.goals_away, my_bet.goals_home, my_bet.goals_away
+                ),
+                match_type.score_factor
+            ) as my_score,
+
+            (
+                calc_bet_score(
+                    calc_bet_result(
+                        match.goals_home, match.goals_away, my_bet.goals_home, my_bet.goals_away
+                    ),
+                    match_type.score_factor
+                )
+                -
+                (
+                    select round(avg(calc_bet_score(
+                        calc_bet_result(
+                            match.goals_home, match.goals_away, friend_bet.goals_home, friend_bet.goals_away
+                        ),
+                        match_type.score_factor
+                        )), 1)
+                    from friend
+                    join bet as friend_bet on (friend_bet.user_id = friend.to_user_id)
+                    where friend.from_user_id = :id
+                )
+            ) as my_gain
+
             FROM match
-             -- No LEFT JOIN here to discard matches without bets (and prevent division by zero)
-            JOIN bet ON match.id = bet.match_id
-            JOIN user_account ON user_account.id = bet.user_id
+            JOIN match_type ON (match_type.id = match.match_type_id)
+            JOIN team as home_team ON (home_team.id = match.home_team_id)
+            JOIN team as away_team ON (away_team.id = match.away_team_id)
+            LEFT JOIN bet as my_bet ON (my_bet.match_id = match.id AND my_bet.user_id = :id)
             WHERE now() > match.starts_at AND match.goals_home IS NOT NULL AND match.goals_away IS NOT NULL
-            GROUP BY match.id
-            ORDER BY match.starts_at DESC
+            ORDER BY ${orderBy}
         `,
         {
             id: req.user ? req.user.id : 0,
@@ -44,19 +107,20 @@ router.get("/past", async (req, res) => {
         }
     );
 
-    for (var i = 0; i < matches.rows.length; i++) {
-        var match = matches.rows[i];
-        match.draw = 100 - match.winnerhome - match.winneraway;
-        var bets = { correct: [], diff: [], winner: [], wrong: [] };
-        for (var j = 0; j < match.listresult.length; j++) {
-            bets[match.listresult[j]].push({
-                name: match.listname[j],
-                goalsHome: match.listhome[j],
-                goalsAway: match.listaway[j],
-                id: match.listid[j],
-                friend: match.listfriends[j],
-                me: match.listme[j],
-            });
+    for (const match of matches.rows) {
+        match.percent = {
+            home: Math.round(
+                (match.bet_counts.home / match.bet_counts.total) * 100
+            ),
+            away: Math.round(
+                (match.bet_counts.away / match.bet_counts.total) * 100
+            ),
+        };
+        match.percent.draw = 100 - match.percent.home - match.percent.away;
+
+        const bets = { correct: [], diff: [], winner: [], wrong: [] };
+        for (const bet of match.all_bets) {
+            bets[bet.result].push(bet);
         }
         match.bets = bets;
     }
@@ -68,5 +132,9 @@ router.get("/past", async (req, res) => {
             .where({ id: req.user.id });
     }
 
-    res.render("past", { matches: matches.rows });
+    res.render("past", {
+        matches: matches.rows,
+        is_logged_in: !!req.user,
+        order_by_gain: orderByGain,
+    });
 });
