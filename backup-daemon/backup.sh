184d01a52337bf75a480d85ca5d29045f8ec623e
Hans Kirchner <hans-kirchner@gmx.de>
2022-11-12 00:54:45 +0100

    Update docker images

--- backup-daemon/backup.sh
+++ backup-daemon/backup.sh
@@ -3,4 +3,4 @@ set -e
 
 NOW=$(date +'%Y%m%d_%H%M%S')
 BACKUP_FILE="/backups/db_$NOW.sql.gz"
-PGPASSWORD=123456 pg_dump -h db -U euro2020 euro2020 | gzip > "$BACKUP_FILE"
+PGPASSWORD=123456 pg_dump -h db -U cup2022 cup2022 | gzip > "$BACKUP_FILE"
