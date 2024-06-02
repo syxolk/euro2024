6d470b1a52d681087748b0f6af43d7dd7ab902ed
Hans Kirchner <hans-kirchner@gmx.de>
2024-06-02 23:08:48 +0200

    Fix name of database for backups

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     origin: process.env.ORIGIN || "http://localhost:8080",
     db: {
-        username: process.env.DB_USER || "cup2022",
+        username: process.env.DB_USER || "cup2024",
         password: process.env.DB_PASSWORD || "123456",
         host: process.env.DB_HOST || "localhost",
         //port: process.env.DB_PORT || 5432,
-        database: process.env.DB_NAME || "cup2022",
+        database: process.env.DB_NAME || "cup2024",
         dialect: "postgres",
     },
 
