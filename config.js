c039acda2dca304f7668af497a3a2dd290f1b084
Hans Kirchner <hans-kirchner@gmx.de>
2024-06-03 08:53:22 +0200

    Remove unused template.config.coffee and config.db

--- config.js
+++ config.js
@@ -1,13 +1,5 @@
 module.exports = {
     origin: process.env.ORIGIN || "http://localhost:8080",
-    db: {
-        username: process.env.DB_USER || "cup2024",
-        password: process.env.DB_PASSWORD || "123456",
-        host: process.env.DB_HOST || "localhost",
-        //port: process.env.DB_PORT || 5432,
-        database: process.env.DB_NAME || "cup2024",
-        dialect: "postgres",
-    },
 
     httpPort: process.env.PORT || 8080,
     sessionSecret: process.env.SESSION_SECRET || "octocat",
