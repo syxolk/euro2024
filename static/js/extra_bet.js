30f16a6f328d1cd9b0d5aa2e1abf2d9f9072a8a2
Hans Kirchner <hans-kirchner@gmx.de>
2024-06-08 17:56:56 +0200

    Add translations for extra bets

--- static/js/extra_bet.js
+++ static/js/extra_bet.js
@@ -4,7 +4,7 @@
 
         const maxTeams = parseInt($("input[name=max_teams]", form).val(), 10);
 
-        $("input:checkbox", form).on("change", function () {
+        function updateView() {
             const checkedBoxes = $("input:checkbox:checked", form).length;
 
             if (checkedBoxes <= maxTeams) {
@@ -14,6 +14,14 @@
                 $("button[type=submit]", form).prop("disabled", true);
                 $(".max-teams-warning", form).removeClass("d-none");
             }
+        }
+
+        // Browsers tend to memorize form values -> need to update the view immediately
+        updateView();
+
+        $("input:checkbox", form).on("change", function () {
+            // update the view whenever a checkbox is clicked
+            updateView();
         });
     });
 })();
