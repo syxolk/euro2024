15f292a599eb4bbc715fa3249337ebc27f743cc3
Hans Kirchner <h.kirchner@qplaner.de>
2026-05-27 00:00:27 +0200

    Re-add comments in knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -33,10 +33,24 @@ export const production: Knex.Config = {
         application_name: "knex-production",
     },
     pool: {
+        // See here for all config options:
+        // https://github.com/vincit/tarn.js/
+
         min: 2,
         max: 10,
+
+        // acquire promises are rejected after this many milliseconds
+        // if a resource cannot be acquired
         acquireTimeoutMillis: 30000,
+
+        // create operations are cancelled after this many milliseconds
+        // if a resource cannot be acquired
         createTimeoutMillis: 30000,
+
+        // If true, when a create fails, the first pending acquire is
+        // rejected with the error. If this is false (the default) then
+        // create is retried until acquireTimeoutMillis milliseconds has
+        // passed.
         propagateCreateError: false,
     },
     migrations: {
